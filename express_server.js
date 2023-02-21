41e797460f3c32beccbee37c76be5ab8916d096a
YuqinHu <h878688736@gmail.com>
2023-02-20 18:16:32 -0800

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -4,10 +4,24 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+function generateRandomString() {
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let string = '';
+
+  for (let i = 0; i < 6; i++) {
+    string += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+
+  return string;
+}
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -27,10 +41,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: "https://www.google.com/" };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
